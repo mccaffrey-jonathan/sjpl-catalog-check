e7a1df549e21448340941fc591d8fafff1e58402
Jon McCaffrey <jmccaffrey@nvidia.com>
2014-05-12 15:55:11 -0700

    More reliable keyword check

--- amz-tab-book-query.js
+++ amz-tab-book-query.js
@@ -2,8 +2,11 @@ function checkAmzBookPage() {
     var metas = document.getElementsByTagName('meta');
 
     for (i=0; i < metas.length; i++) {
-        if (metas[i].getAttribute('name') == 'title') {
-            return metas[i].getAttribute('content').split(':')[0];
+        // if (metas[i].getAttribute('name') == 'title') {
+        //     return metas[i].getAttribute('content').split(':')[0];
+        // }
+        if (metas[i].getAttribute('name') == 'keywords') {
+            return metas[i].getAttribute('content');
         }
     }
 
